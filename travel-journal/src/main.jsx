3bf94dba10befad13162587497ace34ad0b19644
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2022-12-23 16:40:03 -0600

    Put everything into the App component

--- travel-journal/src/main.jsx
+++ travel-journal/src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Header from "./Header";
-import Card from "./Card";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <Card />
+    <App />
   </React.StrictMode>
 );
