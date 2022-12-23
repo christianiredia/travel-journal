233b40a8c52fc0c47ba7b3725735ed7e4999a805
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2022-12-23 14:55:06 -0600

    Updated content

--- travel-journal/src/main.jsx
+++ travel-journal/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Header from "./Header";
+import Card from "./Card";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Header />
+    <Card />
+  </React.StrictMode>
+);
