0cf28c58ccf46c47a252b47c5821dc6d57b8d9cf
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2022-12-23 13:36:23 -0600

    Added the data content that will be rendered

---
+++ travel-journal/src/data.jsx
@@ -0,0 +1,36 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "JAPAN",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "AUSTRAILIA",
+    googleMapsUrl:
+      "https://www.google.com/maps/d/u/0/viewer?mid=1NYBTZlF3StQEf65_sVbpMlskCoo&hl=en&ll=-33.85691%2C151.215262&z=17",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    imageUrl:
+      "https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80",
+  },
+  {
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapsUrl:
+      "https://www.google.com/maps/d/u/0/viewer?hl=en_US&mid=1zi9bW9Zkl8ExIwx-ZNbeChUSK38&ll=62.09137699999997%2C7.061932999999994&z=17",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1587589396598-433f04c9623b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+];
