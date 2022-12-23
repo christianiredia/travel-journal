ecb0e679e9fe8ceb70265055c280756391a274b1
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2022-12-23 16:39:12 -0600

    looped through data array to render to dom

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import Card from "./Card";
+import data from "./data";
+import Header from "./Header";
 
 function App() {
-
+  const cards = data.map((item) => {
+    return (
+      <Card
+        title={item.title}
+        location={item.location}
+        googleMapsUrl={item.googleMapsUrl}
+        startDate={item.startDate}
+        endDate={item.endDate}
+        description={item.description}
+        imageUrl={item.imageUrl}
+      />
+    );
+  });
   return (
-
-  )
+    <div>
+      <Header />
+      {cards}
+    </div>
+  );
 }
 
-export default App
+export default App;
